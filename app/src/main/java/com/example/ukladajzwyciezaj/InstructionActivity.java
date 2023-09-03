3c5b6e74925b9aadd8455e343ca7c09c27ff9e9a
DominikBabiarczyk <babiarczyk@student.agh.edu.pl>
2023-09-03 13:30:50 +0200

    j

--- app/src/main/java/com/example/ukladajzwyciezaj/InstructionActivity.java
+++ app/src/main/java/com/example/ukladajzwyciezaj/InstructionActivity.java
@@ -11,12 +11,17 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class InstructionActivity extends AppCompatActivity {
-    int[] pictureCards = new int[9];
-    String[] descriptionCards = {"a", "v", "f", "w", "b", "m", "q", "d", "b"};
 
     int[] pictureFunctionaly = new int[9];
     String[] descriptionFunctionaly = {"a", "v", "f", "w", "b", "m", "q", "d", "b"};
@@ -28,14 +33,48 @@ public class InstructionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
-        for (int j=0; j<descriptionCards.length; j++){
-            pictureCards[j] = getResources().getIdentifier("grafika_karty", "drawable", getPackageName());
+
+        InputStream inputStream = this.getResources().openRawResource(R.raw.set_information_about_kart);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> description = new ArrayList<>();
+        ArrayList<Integer> picture = new ArrayList<>();
+
+        while (true) {
+            try {
+                if (!((line = bufferedReader.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String[] actualValue = line.split(";");
+            stringBuilder.append(line);
+            if (actualValue[0].equals("----name_grafic----")) {
+                continue;
+            }
+            name.add(actualValue[0]);
+            description.add(actualValue[1]);
+            picture.add(getResources().getIdentifier(actualValue[2], "drawable", getPackageName()));
         }
+
+        /*
+        int[] pictureCards = new int[name.size()];
+        String[] descriptionCards = new String[name.size()];
+        String[] nameCards = new String[name.size()];
+         */
+        int[] pictureCards = new int[picture.size()];
+        for (int j=0; j<description.size(); j++){
+            pictureCards[j] = picture.get(j);
+        }
+
 
         for (int j=0; j<descriptionFunctionaly.length; j++){
             pictureFunctionaly[j] = getResources().getIdentifier("kart_to_insite","drawable", getPackageName());
         }
-        adapterCards = new ListInstructionAdapter(this, pictureCards, descriptionCards);
+        adapterCards = new ListInstructionAdapter(this, pictureCards, description.toArray(new String[description.size()]));
         adapterFunctionaly = new ListInstructionAdapter(this, pictureFunctionaly, descriptionFunctionaly);
     }
 
